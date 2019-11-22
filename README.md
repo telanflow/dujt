97da618388467e8c6160d4fe9f3fdd2d1b9e2383
telan <ziiber@foxmail.com>
2019-11-22 12:21:21 +0800

    [fix] Readme

--- README.md
+++ README.md
@@ -1,2 +1,27 @@
-# dujt
-毒鸡汤 - 壮士可要来一碗！
+# 毒鸡汤 - Go
+壮士可要来一碗！
+
+# 安装
+[Download](https://github.com/telanflow/dujt/releases)
+
+# Usage
+```
+➜  ./darwin_amd64 -h
+
+Usage of ./darwin_amd64:
+  -addr string
+        服务地址 (default "0.0.0.0")
+  -port string
+        服务端口 (default "80")
+``` 
+
+# 变量含义 (web/index.html)
+    url: http://localhost
+    host: localhost
+    line: 毒鸡汤语句
+
+# 预览图
+![screenshot](screenshot.png)
+
+# 感谢
+[原版毒鸡汤 PHP](https://github.com/egotong/nows)
--- build.sh
+++ build.sh
@@ -1,5 +1,9 @@
 #!/usr/bin/env bash
 
-CGO_ENABLED=0 GOOS=linux GOARCH=amd64 go build -o ./Dujt_linux_amd64 ./Dujt.go
-CGO_ENABLED=0 GOOS=darwin GOARCH=amd64 go build -o ./Dujt_darwin_amd64 ./Dujt.go
-CGO_ENABLED=0 GOOS=windows GOARCH=amd64 go build -o ./Dujt_windows_amd64.exe ./Dujt.go
+CGO_ENABLED=0 GOOS=linux GOARCH=amd64 go build -o ./linux_amd64 ./Dujt.go
+CGO_ENABLED=0 GOOS=darwin GOARCH=amd64 go build -o ./darwin_amd64 ./Dujt.go
+CGO_ENABLED=0 GOOS=windows GOARCH=amd64 go build -o ./windows_amd64.exe ./Dujt.go
+
+CGO_ENABLED=0 GOOS=linux GOARCH=arm GOARM=5 go build -o ./linux_armv5 ./Dujt.go
+CGO_ENABLED=0 GOOS=linux GOARCH=arm GOARM=6 go build -o ./linux_armv6 ./Dujt.go
+CGO_ENABLED=0 GOOS=linux GOARCH=arm GOARM=7 go build -o ./linux_armv7 ./Dujt.go
